217a1d8cd0b54c38e57e3d7c338fd46c769c908e
Clément <clem_0339@hotmail.com>
2020-04-07 12:07:15 +0200

    Ajouts classes clément

--- sopra-trottinette/src/main/java/model/Administrateur.java
+++ sopra-trottinette/src/main/java/model/Administrateur.java
@@ -1,5 +1,31 @@
 package model;
 
-public class Administrateur {
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("admin")
+public class Administrateur extends Utilisateur{
+	@Column(name = "first_name", length = 100)
+	private String prenom;
+
+	public Administrateur() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+	
+	
 
 }
--- sopra-trottinette/src/main/java/model/FinDeTrajet.java
+++ sopra-trottinette/src/main/java/model/FinDeTrajet.java
@@ -1,5 +1,78 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Version;
+
+@Entity
+@DiscriminatorValue("end_of_journey")
 public class FinDeTrajet {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	@Version
+	private int version;
+	@Column(name = "picture", length = 255)
+	private String photo;
+	@Column(name = "comment", length = 400)
+	private String commentaire;
+	@ManyToOne
+	@JoinColumn(name="reservation_id")
+	private Reservation reservation;
+
+	public FinDeTrajet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public String getCommentaire() {
+		return commentaire;
+	}
+
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
+
+	public Reservation getReservation() {
+		return reservation;
+	}
+
+	public void setReservation(Reservation reservation) {
+		this.reservation = reservation;
+	}
+	
+	
+	
+	
 
 }
