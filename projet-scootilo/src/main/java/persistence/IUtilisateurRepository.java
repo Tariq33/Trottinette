ba62e09e712fd2a16386ef34961cc3ec0e6abeed
Thomas <pouet>
2020-05-13 13:40:40 +0200

    imports

--- projet-scootilo/src/main/java/persistence/IUtilisateurRepository.java
+++ projet-scootilo/src/main/java/persistence/IUtilisateurRepository.java
@@ -1,6 +1,8 @@
 package persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import model.Utilisateur;
 
