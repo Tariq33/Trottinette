8b902c6263c1115a4e213d44839404fda35ac57c
Greg <moi!>
2020-05-17 17:15:45 +0200

    ajout dans IReservation

--- projet-scootilo/src/main/java/persistence/IReservationRepository.java
+++ projet-scootilo/src/main/java/persistence/IReservationRepository.java
@@ -13,4 +13,7 @@ public interface IReservationRepository extends JpaRepository<Reservation, Long>
 	@Query("select r from Client c join c.reservations r where c.identifiant = :identifiant and r.expiree = TRUE")
 	List<Reservation> FindReservationExpireeByIdentifiant(@Param("identifiant") String identifiant);
 	
+//	@Query("select r from Client c join c.reservations r where c.identifiant = :identifiant and r.expiree = FALSE")
+//	List<Reservation> FindReservationEnCoursByIdentifiant(@Param("identifiant") String identifiant);
+	
 }
