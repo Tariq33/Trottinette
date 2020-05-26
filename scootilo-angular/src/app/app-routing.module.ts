1223c6fbd52be777388cd7e449b9f2b594078077
Greg <moi!>
2020-05-26 15:22:21 +0200

    Auto stash before merge of "master" and "origin/master"

--- scootilo-angular/src/app/app-routing.module.ts
+++ scootilo-angular/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AccueilComponent} from "./components/accueil/accueil.component";
+import {PageDaccueilComponent} from "./components/page-daccueil/page-daccueil.component";
 
 const routes: Routes = [
-  {path: "", component: AccueilComponent},
-  {path: "", component: AccueilComponent},
-  {path: "", component: AccueilComponent}
+  {path: "", component: PageDaccueilComponent},
+  {path: "", component: PageDaccueilComponent},
+  {path: "", component: PageDaccueilComponent}
 ];
 
 @NgModule({
