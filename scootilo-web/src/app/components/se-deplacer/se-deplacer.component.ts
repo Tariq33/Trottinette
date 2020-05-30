af5049ad3ee6d59fafd376f1a019a49f85796c9e
Thomas <pouet>
2020-05-31 00:12:52 +0200

    affichage transports dynamique sur carte selon préférences cochées

--- scootilo-web/src/app/components/se-deplacer/se-deplacer.component.ts
+++ scootilo-web/src/app/components/se-deplacer/se-deplacer.component.ts
@@ -7,6 +7,8 @@ import {ClientService} from "../../service/client.service";
 import {SessionService} from "../../service/session.service";
 import {Adresse} from "../../model/adresse";
 import {AdresseService} from "../../service/adresse.service";
+import {control} from "leaflet";
+import layers = control.layers;
 
 
 @Component({
@@ -27,6 +29,9 @@ export class SeDeplacerComponent implements OnInit {
   moyensDeTransportObs: Array<MoyenDeTransport> = new Array<MoyenDeTransport>();
   client: Client = new Client();
   ongletReservationShow: boolean = false;
+  trotMarkers = new L.LayerGroup();
+  scootMarkers = new L.LayerGroup();
+  veloMarkers = new L.LayerGroup();
 
   veloIcon = new L.Icon({
     iconUrl: '../../../assets/icon-velo.png'
@@ -60,6 +65,48 @@ export class SeDeplacerComponent implements OnInit {
       if(adr.nomAdresse==nom){
         this.adrArrivee=adr.rue+" "+adr.codePostal+" "+adr.ville;
         return;
+      }
+    }
+  }
+
+  charger3(bool:boolean, type: string){
+    if(type=="trottinette"){
+      this.client.preference.trottinette = bool;
+      if(!bool) {
+        for (let i = 0; i < this.trotMarkers.getLayers().length; i++) {
+          this.trotMarkers.getLayers()[i].removeFrom(this.map);
+        }
+      }
+      else{
+        for (let i = 0; i < this.trotMarkers.getLayers().length; i++) {
+          this.trotMarkers.getLayers()[i].addTo(this.map);
+        }
+      }
+    }
+    else if(type=="velo"){
+      this.client.preference.velo = bool;
+      if(!bool) {
+        for (let i = 0; i < this.veloMarkers.getLayers().length; i++) {
+          this.veloMarkers.getLayers()[i].removeFrom(this.map);
+        }
+      }
+      else{
+        for (let i = 0; i < this.veloMarkers.getLayers().length; i++) {
+          this.veloMarkers.getLayers()[i].addTo(this.map);
+        }
+      }
+    }
+    else{
+      this.client.preference.scooter = bool;
+      if(!bool) {
+        for (let i = 0; i < this.scootMarkers.getLayers().length; i++) {
+          this.scootMarkers.getLayers()[i].removeFrom(this.map);
+        }
+      }
+      else{
+        for (let i = 0; i < this.scootMarkers.getLayers().length; i++) {
+          this.scootMarkers.getLayers()[i].addTo(this.map);
+        }
       }
     }
   }
@@ -105,6 +152,9 @@ export class SeDeplacerComponent implements OnInit {
     var container = L.DomUtil.get('map');
     if (container.style.position.valueOf() == "") {
       this.map = L.map('map', {center: [centre.lat, centre.lng], zoom: zoomLevel});
+      this.map.addLayer(this.trotMarkers);
+      this.map.addLayer(this.scootMarkers);
+      this.map.addLayer(this.veloMarkers);
     }
 
     const mainLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
@@ -140,73 +190,50 @@ export class SeDeplacerComponent implements OnInit {
   }
 
  addMarker(transport){
-   if(this.sessionService.getClient().type!="customer"){
-
-     if (transport.typeDeTransport == "velo") {
-       const marker = L.marker([transport.latitude, transport.longitude], {icon: this.veloIcon});
-       marker.addTo(this.map);
-       marker.on("click", event => {
-         console.log("VELO");
-         this.isShow();
-         this.getTransportClick(transport);
-       })
-       marker.bindPopup('<h1>Velo</h1>');
-     } else if (transport.typeDeTransport == "scooter") {
-       const marker = L.marker([transport.latitude, transport.longitude], {icon: this.scootIcon});
-       marker.addTo(this.map);
-       marker.on("click", event => {
-         console.log("SCOOTER");
-         this.isShow();
-         this.getTransportClick(transport);
-       })
-       marker.bindPopup('<h1>Scooter</h1>');
-     } else {
-       const marker = L.marker([transport.latitude, transport.longitude], {icon: this.trotIcon});
-       marker.addTo(this.map);
-       marker.on("click", event => {
-         console.log("TROTTINETTE");
-         this.isShow();
-         this.getTransportClick(transport);
-       })
-       marker.bindPopup('<h1>Trottinette</h1>');
+   if (transport.typeDeTransport == "velo") {
+     const marker = L.marker([transport.latitude, transport.longitude], {icon: this.veloIcon});
+     marker.addTo(this.veloMarkers);
+     marker.on("click", event => {
+       this.isShow();
+       this.getTransportClick(transport);
+     })
+     marker.bindPopup('<h1>Velo</h1>');
+     if(this.sessionService.getClient().type=="customer"){
+        if(!this.client.preference.velo){
+          this.veloMarkers.getLayers()[this.veloMarkers.getLayers().length-1].removeFrom(this.map);
+        }
      }
-
    }
-   else{
-
-     if ((transport.typeDeTransport == "velo" && this.client.preference.velo) || (this.client.preference.velo==false && this.client.preference.scooter==false && this.client.preference.trottinette==false)) {
-       const marker = L.marker([transport.latitude, transport.longitude], {icon: this.veloIcon});
-       marker.addTo(this.map);
-       marker.on("click", event => {
-         console.log("VELO");
-         //this.isShowItineraire();
-         this.isShow();
-         this.getTransportClick(transport);
-       })
-       marker.bindPopup('<h1>Velo</h1>');
-     } else if ((transport.typeDeTransport === "scooter" && this.client.preference.scooter) || (this.client.preference.velo==false && this.client.preference.scooter==false && this.client.preference.trottinette==false)) {
-       const marker = L.marker([transport.latitude, transport.longitude], {icon: this.scootIcon});
-       marker.addTo(this.map);
-       marker.on("click", event => {
-         console.log("SCOOTER");
-         this.isShow();
-         this.getTransportClick(transport);
-       })
-       marker.bindPopup('<h1>Scooter</h1>');
-     } else if((transport.typeDeTransport === "trottinette" && this.client.preference.trottinette) || (this.client.preference.velo==false && this.client.preference.scooter==false && this.client.preference.trottinette==false)){
-       const marker = L.marker([transport.latitude, transport.longitude], {icon: this.trotIcon});
-       marker.addTo(this.map);
-       marker.on("click", event => {
-         console.log("TROTTINETTE");
-         this.isShow();
-         this.getTransportClick(transport);
-       })
-       marker.bindPopup('<h1>Trottinette</h1>');
+   else if (transport.typeDeTransport == "scooter") {
+     const marker = L.marker([transport.latitude, transport.longitude], {icon: this.scootIcon});
+     marker.addTo(this.scootMarkers);
+     marker.on("click", event => {
+       this.isShow();
+       this.getTransportClick(transport);
+     })
+     marker.bindPopup('<h1>Scooter</h1>');
+     if(this.sessionService.getClient().type=="customer"){
+       if(!this.client.preference.scooter){
+         this.scootMarkers.getLayers()[this.scootMarkers.getLayers().length-1].removeFrom(this.map);
+       }
      }
-
+   }
+   else {
+     const marker = L.marker([transport.latitude, transport.longitude], {icon: this.trotIcon});
+     marker.addTo(this.trotMarkers);
+     marker.on("click", event => {
+       this.isShow();
+       this.getTransportClick(transport);
+     })
+     marker.bindPopup('<h1>Trottinette</h1>');
+     if(this.sessionService.getClient().type=="customer"){
+       if(!this.client.preference.trottinette){
+         this.trotMarkers.getLayers()[this.trotMarkers.getLayers().length-1].removeFrom(this.map);
+       }
+     }
    }
 
-  }
+ }
 
   getTransportClick(transport) {
     this.moyenTransportClick = transport;
