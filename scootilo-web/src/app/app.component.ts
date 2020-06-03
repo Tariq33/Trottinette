82366f55f69f8557641ffae626000b2422703be3
Tariq <tariq.farud.sigma@gmail.com>
2020-06-03 17:43:23 +0200

    les bords en mode sombre

--- scootilo-web/src/app/app.component.ts
+++ scootilo-web/src/app/app.component.ts
@@ -84,7 +84,7 @@ export class AppComponent {
   }
 
   themeSombre() {
-    document.documentElement.style.setProperty('--border-color', "grey");
+    document.documentElement.style.setProperty('--border-color', "white");
     document.documentElement.style.setProperty('--bg-color', "grey");
     document.documentElement.style.setProperty('--img-accueil', "url('/assets/bg2.jpg')");
     document.documentElement.style.setProperty('--bg-navbar', "grey");
